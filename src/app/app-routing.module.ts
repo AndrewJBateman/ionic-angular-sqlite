8d88aeb35856975b8550a7aa152b716865248666
AndrewJBateman <gomezbateman@yahoo.com>
2021-05-24 15:16:57 +0200

    chore: updated to Angular v12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'developers', pathMatch: 'full' },
-  { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
-  { path: 'developers/:id', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
+  { path: 'developers', loadChildren: () => import('./pages/developers/developers.module').then(m => m.DevelopersPageModule) },
+  { path: 'developers/:id', loadChildren: () => import('./pages/developer/developer.module').then(m => m.DeveloperPageModule) },
 ];
 
 @NgModule({
