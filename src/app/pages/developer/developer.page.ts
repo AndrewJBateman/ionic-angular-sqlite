12a8f97080c28df07012506a626252ebceaa1df1
AndrewJBateman <gomezbateman@yahoo.com>
2021-05-25 11:24:18 +0200

    chore: formatting & dependency updates

--- src/app/pages/developer/developer.page.ts
+++ src/app/pages/developer/developer.page.ts
@@ -1,47 +1,52 @@
-import { DatabaseService, Dev } from './../../services/database.service';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
+import { DatabaseService, Dev } from "./../../services/database.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-	selector: 'app-developer',
-	templateUrl: './developer.page.html',
-	styleUrls: ['./developer.page.scss'],
+  selector: "app-developer",
+  templateUrl: "./developer.page.html",
+  styleUrls: ["./developer.page.scss"],
 })
 export class DeveloperPage implements OnInit {
-	developer: Dev = null;
-	skills = '';
+  developer: Dev = null;
+  skills = "";
 
-	constructor(private route: ActivatedRoute, private db: DatabaseService, private router: Router, private toast: ToastController) { }
+  constructor(
+    private route: ActivatedRoute,
+    private db: DatabaseService,
+    private router: Router,
+    private toast: ToastController
+  ) {}
 
-	ngOnInit() {
-		this.route.paramMap.subscribe(params => {
-			const devId = params.get('id');
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const devId = params.get("id");
 
-			this.db.getDeveloper(devId).then(data => {
-				this.developer = data;
-				this.skills = this.developer.skills.join(',');
-			});
-		});
-	}
+      this.db.getDeveloper(+devId).then((data) => {
+        this.developer = data;
+        this.skills = this.developer.skills.join(",");
+      });
+    });
+  }
 
-	delete() {
-		this.db.deleteDeveloper(this.developer.id).then(() => {
-			this.router.navigateByUrl('/');
-		});
-	}
+  delete() {
+    this.db.deleteDeveloper(this.developer.id).then(() => {
+      this.router.navigateByUrl("/");
+    });
+  }
 
-	updateDeveloper() {
-		let skills = this.skills.split(',');
-		skills = skills.map(skill => skill.trim());
-		this.developer.skills = skills;
+  updateDeveloper(): void {
+    let skills = this.skills.split(",");
+    skills = skills.map((skill) => skill.trim());
+    this.developer.skills = skills;
 
-		this.db.updateDeveloper(this.developer).then(async (res) => {
-			const toast = await this.toast.create({
-				message: 'Developer updated',
-				duration: 3000
-			});
-			toast.present();
-		});
-	}
+    this.db.updateDeveloper(this.developer).then(async (res) => {
+      const toast = await this.toast.create({
+        message: "Developer updated",
+        duration: 3000,
+      });
+      toast.present();
+    });
+  }
 }
